bb433925f9264e0b16881f72881ff037dd97fcf4
Hadrian <157500642+HadrianBez@users.noreply.github.com>
2024-02-05 12:12:11 +0200

    Update css4p02.py

--- css4p02.py
+++ css4p02.py
@@ -78,19 +78,8 @@ events = {
     "dblclick":"function(params) { return [params.type, params.name, params.value] }"
 }
 
-value = st_echarts(options=options, events=events)
+value = st_echarts(options=options,x="Run", events=events)
 st.write(value)  # shows name on bar click and type+name+value on bar double click
-
-
-import altair as alt
-
-chart = (
-        alt.Chart(yline_2D, title='measure of different elements over time').mark_line().encode(
-     x='date', y='value', color='parameter')
-)
-
-st.altair_chart(chart)
-
 
 
 
